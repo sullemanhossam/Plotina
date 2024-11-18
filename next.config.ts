b4f7ad81ac1f886787ae92f405208ea3719d6e8d
sullemanhossam <sullemanhossam4@gmail.com>
2024-11-18 16:48:21 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/your-repo-name',
+  basePath: '/plotina',
 };
 
 export default nextConfig;
